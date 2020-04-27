d9b6b5096c9210e572fe044e431e082ff67a87ef
Akhil Anilkumar <akhil.slipknot@hotmail.com>
2020-04-27 18:53:29 +0530

    Add description to service method.
    Signed-off-by: Akhil Anilkumar <akhil.slipknot@hotmail.com>

--- src/main/java/com/tutorials/springboot/learning/service/DogService.java
+++ src/main/java/com/tutorials/springboot/learning/service/DogService.java
@@ -5,15 +5,48 @@ import com.tutorials.springboot.learning.dto.DogDTO;
 import java.util.List;
 
 public interface DogService {
+
+    /**
+     * Add dog's details
+     *
+     * @param dog DTO
+     */
     void saveDogDetails(DogDTO dog);
 
+    /**
+     * Find dog details by ID
+     *
+     * @param id
+     * @return
+     */
     DogDTO findDogDetail(int id);
 
+    /**
+     * Get all records
+     *
+     * @return
+     */
     List<DogDTO> getDogsDetail();
 
+    /**
+     * Save list of records
+     *
+     * @param dogs
+     */
     void saveAllDogs(List<DogDTO> dogs);
 
+    /**
+     * Delete a record
+     *
+     * @param id
+     */
     void delete(Integer id);
 
+    /**
+     * Update a record
+     *
+     * @param id
+     * @param dog
+     */
     void update(Integer id, DogDTO dog);
 }
